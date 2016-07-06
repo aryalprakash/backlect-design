50b9d1806d0fdb9fca709db5295a91305d988da9
Prakash Aryal <aryalprakas@gmail.com>
2016-07-06 13:26:07 +0545

    app and css

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,47 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div className="bg">
+        <div className="bg">
           <div className="center-box">
             <div className="center-contents">
               <div className="center-base">
-                <div className="logo-box logo center-box-logo">B</div>
-                <div className="title1">Welcome to Backlect</div>
-                <div className="para-text">The best Cloud Database to store your data. Works great for team collaboration and mobile app projects with million of users.</div>
-                <div className="title2">Did you know</div>
-                <div className="para-text">It’s built on Google Cloud Infrastructure
-                Can scale to millions of millions inputs
-                Automatically generates REST APIs</div>
+                <div className="padding30">
+                  <div className="logo-box logo center-box-logo">B</div>
+                  <div className="title1">Welcome to Backlect</div>
+                  <div className="para-text">The best Cloud Database to store your data. Works great for team collaboration and mobile app projects with million of users.</div>
+                  <div className="title2">Did you know</div>
+                  <div className="para-text">It’s built on Google Cloud Infrastructure
+                  Can scale to millions of millions inputs
+                  Automatically generates REST APIs</div>
+                </div>
               </div>
-              <div className="title1">aass</div>
-
+              <div className="center-form">
+                <div className="padding30">
+                  <div className="title1">Create Your Free Account</div>
+                  <form className="signup-form">
+                    <div className="form-label">Email</div>
+                    <input type="text" placeholder="james@backlect.com" className="input-box" />
+                    <div className="form-label">First Name</div>
+                    <div className="hundred form-group">
+                      <div className="fifty-left">
+                        <input type="text" placeholder="Your Name" className="input-box" />
+                      </div>
+                      <div className="fifty-right">
+                        <input type="text" placeholder="Your Surname" className="input-box" />
+                      </div>
+                    </div>
+                    <div className="form-label">Password</div>
+                    <div className="hundred form-group">
+                      <div className="fifty-left">
+                        <input type="password" placeholder="Password" className="input-box" />
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
             </div>
           </div>
-      </div>
+        </div>
     );
   }
 }
