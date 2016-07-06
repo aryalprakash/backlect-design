9c700d6b513cb99fa0fe8db7cc1014ba579b0b4b
Prakash Aryal <aryalprakas@gmail.com>
2016-07-06 12:03:59 +0545

    signup-first

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,22 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div className="bg">
+          <div className="center-box">
+            <div className="center-contents">
+              <div className="center-base">
+                <div className="logo-box logo center-box-logo">B</div>
+                <div className="title1">Welcome to Backlect</div>
+                <div className="para-text">The best Cloud Database to store your data. Works great for team collaboration and mobile app projects with million of users.</div>
+                <div className="title2">Did you know</div>
+                <div className="para-text">It’s built on Google Cloud Infrastructure
+                Can scale to millions of millions inputs
+                Automatically generates REST APIs</div>
+                </div>
+
+            </div>
+          </div>
+      </div>
     );
   }
 }
